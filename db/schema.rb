bf3dba9fe516a0e8cb78c8ed0e09135ced8e6675
m11o <velonica2227@icloud.com>
2020-10-13 17:31:35 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_10_13_075129) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
