b10fded7d3e7e6b9be24807684c6d0246540902c
m11o <velonica2227@icloud.com>
2020-10-13 17:34:51 +0900

    add title column in videos

--- db/migrate/20201013083320_create_videos.rb
+++ db/migrate/20201013083320_create_videos.rb
@@ -1,6 +1,7 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
     create_table :videos do |t|
+      t.string   :title, presence: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_075129) do
+ActiveRecord::Schema.define(version: 2020_10_13_083320) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_10_13_075129) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "videos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
