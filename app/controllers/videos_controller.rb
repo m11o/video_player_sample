2a3edf0a2c1f9dd56d8f77288132ae10ee9c17fa
m11o <velonica2227@icloud.com>
2020-10-13 17:42:04 +0900

    implement vidoes controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,47 @@
 class VideosController < ApplicationController
+  before_action :set_video, only: %i[show edit update destroy]
+
+  def index
+    @videos = Video.all
+  end
+
+  def show; end
+
+  def new
+    @video = Video.new
+  end
+
+  def create
+    @video = Video.new video_params
+    if @video.save
+      redirect_to video_path(@video)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @video.update video_params
+      redirect_to video_path(@video)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @video.destroy
+    redirect_to videos_path
+  end
+
+  private
+
+  def set_video
+    @video = Video.find params[:id]
+  end
+
+  def video_params
+    params.require(:video).permit(:title, :material)
+  end
 end
